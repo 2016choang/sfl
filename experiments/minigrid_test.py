c000876e2fad6ede936434b304daab98cc460daf
Chris Hoang <trungh98@gmail.com>
2019-11-02 22:30:18 -0400

    back to leaky, batch size 64

--- experiments/minigrid_test.py
+++ experiments/minigrid_test.py
@@ -32,10 +32,11 @@ def build_and_train(env_id="MiniGrid-FourRooms-v0", run_ID=0, cuda_idx=None, sna
     )
     lr_schedule_config={'mode': 'step',
                         'step_size': 20000}
-    algo = DSR(min_steps_learn=int(1e3),
-              learning_rate=2e-3,
-              replay_size=int(1e5)
-              )
+    algo = DSR(batch_size=64,
+               min_steps_learn=int(1e3),
+               learning_rate=2e-3,
+               replay_size=int(1e5)
+               )
     agent = GridDsrAgent()
     runner = MinibatchRlEval(
         algo=algo,
