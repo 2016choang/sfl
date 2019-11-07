7789316021b32d0257ccdcf7e9072a31e8c359c6
Chris Hoang <trungh98@gmail.com>
2019-11-07 00:27:53 -0500

    trafe

--- rlpyt/algos/dqn/dsr/dsr.py
+++ rlpyt/algos/dqn/dsr/dsr.py
@@ -185,6 +185,8 @@ class DSR(RlAlgorithm):
         for i in range(self.updates_per_optimize):
             samples_from_replay = self.replay_buffer.sample_batch(self.batch_size)
             self.re_optimizer.zero_grad()
+            if itr > 3000:
+                import pdb; pdb.set_trace()
             re_loss = self.reconstruct_loss(samples_from_replay)
             re_loss.backward()
 
