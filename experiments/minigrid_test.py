f2dbf52a9b2e1e17efa55e2fe282b81fb7384a04
Chris Hoang <trungh98@gmail.com>
2019-11-09 19:41:17 -0500

    Changed learning rate

--- experiments/minigrid_test.py
+++ experiments/minigrid_test.py
@@ -41,7 +41,7 @@ def build_and_train(env_id="MiniGrid-FourRooms-v0", run_ID=0, cuda_idx=None, mod
 
     algo = DSR(batch_size=32,
                min_steps_learn=int(1e3),
-               learning_rate=1e-4,
+               learning_rate=1e-5,
                replay_size=int(1e5),
                learn_re=learn_re
                )
