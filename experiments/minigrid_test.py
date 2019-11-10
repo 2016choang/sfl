2531c79d5e8fbd625b943f04da13308469ca925e
Chris Hoang <trungh98@gmail.com>
2019-11-09 23:40:46 -0500

    1e-7 learning rate

--- experiments/minigrid_test.py
+++ experiments/minigrid_test.py
@@ -41,7 +41,7 @@ def build_and_train(env_id="MiniGrid-FourRooms-v0", run_ID=0, cuda_idx=None, mod
 
     algo = DSR(batch_size=32,
                min_steps_learn=int(1e3),
-               learning_rate=1e-6,
+               learning_rate=1e-7,
                replay_size=int(1e5),
                learn_re=learn_re
                )
@@ -57,7 +57,7 @@ def build_and_train(env_id="MiniGrid-FourRooms-v0", run_ID=0, cuda_idx=None, mod
         algo=algo,
         agent=agent,
         sampler=sampler,
-        n_steps=1e4,
+        n_steps=4e4,
         log_interval_steps=1e3,
         affinity=dict(cuda_idx=cuda_idx),
         seed=seed
